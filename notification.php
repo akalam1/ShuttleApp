b51c3fe59831d69770c717763286812a6f6231a4
Sharanam0904 <123771220+Sharanam0904@users.noreply.github.com>
2023-04-18 23:49:04 -0400

    All files from my side.

---
+++ notification.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+   
+  </style>
+</head>
+<body>
+<form action="webpage.php" method="post">
+  <label for="message">Notification Message:</label>
+  <input type="text" id="message" name="message">
+  <input type="submit" value="Send Notification">
+</form>
+<?php
+ 
+ $message = 0;
+ ?>
+  
+</body>
+</html>
